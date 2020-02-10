b0237d81ae2df72bd26c40d132153a42e53365ec
Tessa Newman-Heggie <tessanewman@eduroam-163-195.dyn.carleton.edu>
2020-02-10 12:50:18 -0600

    string display bugs

--- display_album.c
+++ display_album.c
@@ -15,7 +15,6 @@ void display(char url_query[]){
     char command[120] =  "curl --silent -o album.txt ";
     strcat(command, url_query);
     system(command);
-	//free(url_query);
 	formatAlbum();
 
 }
@@ -31,7 +30,7 @@ int formatAlbum(){
     album = fopen(file_name, "r");
 	
     if (album == NULL){
-        printf("Could not open file %s",file_name);
+        printf("Could not open file %s\n",file_name);
         return 1;
     }
 	//while there's a line in the text file
@@ -40,32 +39,21 @@ int formatAlbum(){
 		//if "id" is in the line, prints the id number
 	    if(is_substring(str, id)){
 			const char *from = str;
-  			char id_name_long[10]; 
-  			strncpy(id_name_long, from+10, 10);
-			
+  			char id_name[10]; 
+  			strncpy(id_name, from+10, 10);
 			//removes comma from id
-			int i = 0;
-			while( (i<10) && (id_name_long[i] != ',') ){
-				i+=1;
-			}
-			char id_name[i];
+			*strchr(id_name, ',') = '\0';
 			
-			strncpy(id_name, id_name_long, i);
 			
-			//tried to find comma and replace w null- doesn't work
-			/*for(int i; i<10; i++){
-				char current_char = id_name[i];
-				if (current_char == ','){
-					id_name[i] = '\0';
-				}
-				printf("i: %c\n", id_name[i]);
-			} */
 			
-			//tried to print each character individually- doesn't work
-			//printf("\nid: %c",id_name[0]);
-			//printf("%c",id_name[1]);
-			//printf("%c\n",id_name[2]);
 			
+			//works:
+			//id_name_long[strlen(id_name_long)-1]= '\0';
+			//id_name_long[strlen(id_name_long)-1]= '\0';
+			
+			
+			
+			//printf("\nstrlen(id_name_long): %lu\n",strlen(id_name_long));
 			printf("\nid: %s\n",id_name);
 			
 		//if "title" is in the line, prints the title
@@ -73,6 +61,8 @@ int formatAlbum(){
 			const char *from = str;
   			char title_name[100]; 
   			strncpy(title_name, from+13, 100);
+			//removes comma from title
+			*strchr(title_name, ',') = '\0';
 			printf("title: %s\n",title_name);
 		}
 	}
